06d8e6ef3692995e74fb012b5edbb51e91ebaceb
Divs-Git <divyansh.sri258@gmail.com>
2025-07-16 19:30:20 +0530

    refactered models from entity service

--- src/main/java/com/example/uberprojectauthservice/services/AuthService.java
+++ src/main/java/com/example/uberprojectauthservice/services/AuthService.java
@@ -2,7 +2,6 @@ package com.example.uberprojectauthservice.services;
 
 import com.example.uberprojectauthservice.dto.PassengerDto;
 import com.example.uberprojectauthservice.dto.PassengerSignupRequestDto;
-import com.example.uberprojectauthservice.models.Passenger;
 import org.springframework.stereotype.Service;
 
 @Service
